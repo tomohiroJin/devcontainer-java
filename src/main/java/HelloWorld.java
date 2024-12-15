7fcd9b87bd2b37a9b2422900abed855d2e8634e3
tomohiro.jin <tomohiro.jin@hotmail.co.jp>
2024-12-15 11:32:17 +0000

    update: 使っていない処理を削除 add: vscode の辞書を追加

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,10 +1,6 @@
 
 public class HelloWorld {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-
     public static int add(int a, int b) {
         return a + b;
     }
