51883e010f52f49cd4cea9abced34ae6d9a08592
tomohirojin <tomohiro.jin@gmail.com>
2026-08-19 23:09:50 +0900

    fix: 保存し忘れを追加

--- src/main/java/buckpal/application/domain/service/SendMoneyService.java
+++ src/main/java/buckpal/application/domain/service/SendMoneyService.java
@@ -1,19 +1,23 @@
 package buckpal.application.domain.service;
 
+import buckpal.application.port.in.SendMoneyCommand;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+
+// (略)
 
 @RequiredArgsConstructor
 @Transactional
 class SendMoneyService implements SendMoneyUseCase {
-
     private final LoadAccountPort loadAccountPort;
     private final UpdateAccountStatePort updateAccountStatePort;
 
     @Override
     public boolean sendMoney(SendMoneyCommand command) {
-        // TODO: ビジネスルールに関する妥当性確認を行う
-        // TODO: モデルの状態を変える
-        // TODO: 処理結果を返す
+        // TODO ビジネスルールに関する妥当性確認を行う
+        // TODO モデルの状態を変える
+        // TODO 処理結果を返す
     }
+
+    // (略)
 }
